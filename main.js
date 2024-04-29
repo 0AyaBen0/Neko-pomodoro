9f75bec2100d6a180468386ac62c91e647246884
Aya Bendahmane <102223976+0AyaBen0@users.noreply.github.com>
2024-04-29 15:55:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// variables
-
 let workTitle = document.getElementById('work');
 let breakTitle = document.getElementById('break');
 
@@ -11,28 +9,22 @@ let seconds = "59";
 let audio = new Audio("audio/Meow Sound Effect.mp3");
 let play = document.getElementById('start');
 
-// display
 window.onload = () => {
     document.getElementById('minutes').innerHTML = workTime;
     document.getElementById('seconds').innerHTML = seconds;
 
     workTitle.classList.add('active');
 
-    audio.play().then(() => { // pause directly
+    audio.play().then(() => {
         audio.pause();
         audio.currentTime = 0;
       });
-
-
 }
 
-// start timer
 function start() {
-    // change button
     document.getElementById('start').style.display = "none";
     document.getElementById('reset').style.display = "block";
 
-    // change the time
     seconds = 59;
     
     let workMinutes = workTime;
@@ -40,21 +32,15 @@ function start() {
 
     breakCount = 0;
 
-    // countdown
     let timerFunction = () => {
-        //change the display
         seconds = seconds - 1;
         document.getElementById('minutes').innerHTML = workMinutes;
-        document.getElementById('seconds').innerHTML = seconds;
-
-        // start
-        
+        document.getElementById('seconds').innerHTML = seconds;        
 
         if(seconds === 0) {
             workMinutes = workMinutes - 1;
             if(workMinutes === -1 ){
                 if(breakCount % 2 === 0) {
-                    // start break
                     workMinutes = breakMinutes;
                     seconds = 59;
                     breakCount++
@@ -63,7 +49,6 @@ function start() {
                     breakTitle.classList.add('active');
                     
                 }else {
-                    // continue work
                     workMinutes = workTime;
                     seconds = 59;
                     breakCount++
@@ -75,9 +60,6 @@ function start() {
             }
             seconds = 59;
         }
-        
     }
-
-    // start countdown
     setInterval(timerFunction, 1000);
 }
